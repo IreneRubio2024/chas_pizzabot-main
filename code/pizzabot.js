653f940cefd233041e3dbecdcaa9535834232904
Irene Rubio <rubio.hernandez.irene@gmail.com>
2024-11-05 11:51:22 +0100

    Pizzabot

---
+++ code/pizzabot.js
@@ -0,0 +1,46 @@
+const vegetarian = "Vegetarian Pizza";
+const hawaiian = "Hawaiian Pizza";
+const pepperoni = "Pepperoni Pizza";
+
+const pizzaPrice = 80;
+
+//Put your Javscript code here:
+
+alert(
+  `Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`
+);
+
+const orderName = prompt(
+  "Enter the name of the pizza you want to order today."
+);
+
+if (checkOrderName(orderName)) {
+  const orderQuantity = prompt(`How many of ${orderName} do you want?.`);
+  alert(
+    `Great, I'll get started on your ${orderName} right away, it will cost ${totalCost(
+      orderQuantity
+    )}. The pizzas will take ${cookingTime(orderQuantity)}.`
+  );
+}
+
+function checkOrderName(orderName) {
+  return (
+    orderName == "vegetarian" ||
+    orderName == "hawaiian" ||
+    orderName == "pepperoni"
+  );
+}
+
+function totalCost(quantity) {
+  return quantity * pizzaPrice;
+}
+
+function cookingTime(quantity) {
+  if (quantity < 3) {
+    return 10;
+  } else if (quantity > 2 && quantity < 6) {
+    return 15;
+  } else {
+    return 20;
+  }
+}
